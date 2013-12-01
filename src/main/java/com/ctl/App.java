6a3bb5f0adb943eef0076d8f209011f3685ca41a
Cesar Tron-Lozai <cesar.tron-lozai@lombardrisk.com>
2013-12-01 12:13:50 +0000

    Initial commit of maven project

---
+++ src/main/java/com/ctl/App.java
@@ -0,0 +1,13 @@
+package com.ctl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
